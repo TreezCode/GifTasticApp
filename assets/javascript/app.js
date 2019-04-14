18dbf9bbfd893ddee5581c0d4e6436e10b55903f
TreezCode <TreezCode@users.noreply.github.com>
2019-04-13 23:36:52 -0600

    css updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+
+// Create an array of items to act as the original GIF buttons group.
+// Iterate through array. 
+// Add attribute. 
+// Add class. 
+// Render buttons to html by appending to button group
+
